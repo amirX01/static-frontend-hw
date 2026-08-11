0f53e9ae9acc0392a3dc5cdeed9fd3982abe0a23
Amirali Shairi <amirali.shairi@gmail.com>
2026-08-11 18:22:47 +0330

    feat: integrate Footer into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './components/Navbar.css';
+import './components/Footer.css';
 
 function App() {
   return (
     <div>
       <Navbar />
       <h1>Welcome to Frontend HW - Hello React</h1>
+      <Footer />
     </div>
   )
 }
