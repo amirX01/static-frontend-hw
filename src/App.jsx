6ecf13b4ea9990eb72dd3b721e0df99af8842e51
Amirali Shairi <amirali.shairi@gmail.com>
2026-08-11 18:29:09 +0330

    feat: integrate Hero section into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
 import './components/Navbar.css';
 import './components/Footer.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <h1>Welcome to Frontend HW - Hello React</h1>
+      <Hero />
       <Footer />
     </div>
   )
